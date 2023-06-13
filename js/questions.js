f63ad89a0f1ed9e532bd2cee7364d87a68f8b211
Soumyajit Das <114023457+SoumyajitDas142@users.noreply.github.com>
2023-06-13 09:29:27 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,186 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which is a JS library ?",
+    answer: "ReactJS",
+    options: [
+      "Node JS",
+      "ReactJS",
+      "MongoDB",
+      "HTML"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which header file is required in C++ to use OOP ?",
+    answer: "OOP can be used without using any header file",
+    options: [
+      "OOP can be used without using any header file",
+      "stdlib.h",
+      "iostream.h",
+      "stdio.h"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who developed Python Programming Language ?",
+    answer: "Guido van Rossum",
+    options: [
+      "Wick van Rossum",
+      "Rasmus Lerdorf",
+      "Guido van Rossum",
+      "Niene Stom"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which data type in C is used to store whole numbers without decimal points ?",
+    answer: "int",
+    options: [
+      "float",
+      "double",
+      "int",
+      "char"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which keyword is used to declare a variable that cannot be reassigned or modified once assigned ?",
+    answer: "final",
+    options: [
+      "immutable",
+      "final",
+      "const",
+      "static"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?",
+    answer: "Apple",
+    options: [
+      "Commodore",
+      "Apple",
+      "Microsoft",
+      "Atari"
+    ]
+  },
+
+{
+    numb: 12,
+    question: "What does CPU stand for?",
+    answer: "Central Processing Unit",
+    options: [
+      "Central Processing Unit",
+      "Computer Personal Unit",
+      "Central Process Unit",
+      "Central Processor Unit"
+    ]
+  },
+
+{
+    numb: 13,
+    question: "If you were to code software in this language you'd only be able to type 0's and 1's",
+    answer: "Binary",
+    options: [
+      "JavaScript",
+      "Binary",
+      "Python",
+      "C++"
+    ]
+  },
+
+{
+    numb: 14,
+    question: "When Gmail first launched, how much storage did it provide for your email?",
+    answer: "1GB",
+    options: [
+      "5GB",
+      "Unlimited",
+      "512MB",
+      "1GB"
+    ]
+  },
+
+{
+    numb: 15,
+    question: "TThe programming language 'Swift' was created to replace what other programming language?",
+    answer: "Objective-C",
+    options: [
+      "Objective-C",
+      "C#",
+      "Ruby",
+      "C++"
+    ]
+  }
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
